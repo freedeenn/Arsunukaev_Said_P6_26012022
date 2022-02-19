99c667e3ea318fc2804bd999941c137f281eebf5
freedeenn <freedeenn@gmail.com>
2022-02-19 15:22:49 +0100

    commit3

--- back/middleware/access.js
+++ back/middleware/access.js
@@ -1,14 +1,3 @@
-// const user = require("../routes/user");
-// const sauce = require("../routes/sauce");
-// console.log("sauce.user.id");
-// console.log(sauce.userId);
-// exports.canModifySauce = function (user, sauce) {
-// 	return sauce.userId === user.id;
-// };
-
-// // // module.exports = canModifySauce;
-// // module.exports = function (id) {
-// // 	return function (req, res, next)
 const Sauce = require("../models/sauce");
 module.exports = (req, res, next) => {
 	try {
